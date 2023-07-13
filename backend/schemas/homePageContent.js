20f860aea4396de087f72a68f9abdc285e856574
laura-richer <lauraricher@bolser.co.uk>
2023-07-13 16:31:15 +0100

    adding homepage schema

--- backend/schemas/homePageContent.js
+++ backend/schemas/homePageContent.js
@@ -1,27 +1,47 @@
-import {defineField, defineType} from 'sanity'
+import { defineField, defineType, defin } from 'sanity'
 
 export default defineType({
   name: 'homePageContent',
   title: 'Home Page',
   type: 'document',
+  groups: [
+    {
+      name: 'hero',
+      title: 'Hero',
+    },
+    {
+      name: 'about',
+      title: 'About',
+    },
+    {
+      name: 'menu',
+      title: 'Menu',
+    },
+    {
+      name: 'gallery',
+      title: 'Gallery',
+    },
+    {
+      name: 'contact',
+      title: 'Contact',
+    },
+    {
+      name: 'seo',
+      title: 'SEO',
+    },
+  ],
   fields: [
     defineField({
       name: 'title',
       title: 'Title',
       type: 'string',
+      group: 'hero',
     }),
     defineField({
-      name: 'ingredient',
-      title: 'Ingredients',
-      type: 'array',
-      of: [{type: 'reference', to: {type: 'ingredient'}}],
+      name: 'title',
+      title: 'Title',
+      type: 'string',
+      group: 'hero',
     }),
-    defineField({
-      name: 'dietaryRequirement',
-      title: 'Dietary Requirements',
-      type: 'array',
-      of: [{type: 'reference', to: {type: 'dietaryRequirement'}}],
-    }),
-
   ],
 })
--- backend/schemas/index.js
+++ backend/schemas/index.js
@@ -1,8 +1,9 @@
 import blockContent from './blockContent'
+import homePageContent from './homePageContent'
 
 
 import menuItem from './menuItem'
 import ingredient from './ingredient'
 import dietaryRequirement from './dietaryRequirement'
 
-export const schemaTypes = [menuItem, ingredient, dietaryRequirement, blockContent]
+export const schemaTypes = [menuItem, ingredient, dietaryRequirement, homePageContent, blockContent]
