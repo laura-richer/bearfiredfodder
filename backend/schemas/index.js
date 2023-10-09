6b15509ad4f44247d9f278fad2220dc2a2ae0328
laura-richer <lauraricher@bolser.co.uk>
2023-10-09 18:46:37 +0100

    fixing CMS

--- backend/schemas/index.js
+++ backend/schemas/index.js
@@ -1,10 +1,7 @@
-import blockContent from './blockContent'
 import homePageContent from './homePageContent'
-
-
 import menuItem from './menuItem'
 import ingredient from './ingredient'
 import dietaryRequirement from './dietaryRequirement'
 import headerNavigation from './headerNavigation'
 
-export const schemaTypes = [menuItem, ingredient, dietaryRequirement, homePageContent, blockContent, headerNavigation]
+export const schemaTypes = [menuItem, ingredient, dietaryRequirement, homePageContent, headerNavigation]
