1827a23eb0280c8757de2efd6b5be1b666091bc0
laura-richer <lauraricher@bolser.co.uk>
2023-10-10 12:16:11 +0100

    refactoring intersection util function

--- frontend/src/utils/show-section.js
+++ frontend/src/utils/show-section.js
@@ -1,9 +1,15 @@
+const revealElement = targetElement => {
+  targetElement.style.opacity = 1;
+  targetElement.style.transform = 'translateY(0)';
+};
+
 export default (targetElement, callback) => {
   const observer = new IntersectionObserver(
     entries => {
       entries.forEach(entry => {
         if (entry.isIntersecting) {
-          callback();
+          revealElement(targetElement);
+          if (callback) callback();
           observer.unobserve(targetElement);
         }
       });
