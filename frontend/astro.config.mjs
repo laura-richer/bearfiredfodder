1a607e2947e8e7bc0343bc3104cf0db3c7aac8fd
laura-richer <lauraricher@bolser.co.uk>
2023-10-11 13:09:58 +0100

    updating astro

--- frontend/astro.config.mjs
+++ frontend/astro.config.mjs
@@ -1,11 +1,5 @@
 import { defineConfig } from 'astro/config';
-import vercel from '@astrojs/vercel/serverless';
 
 export default defineConfig({
-  output: 'server',
   compressHTML: true,
-  adapter: vercel(),
-  server: {
-    host: true,
-  },
 });
