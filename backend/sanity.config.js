8ea737cdeac3d3fe7fd404d0e92fe3e845f84758
laura-richer <lauraricher@bolser.co.uk>
2024-02-28 22:30:35 +0000

    sanity deploy tool

--- backend/sanity.config.js
+++ backend/sanity.config.js
@@ -1,14 +1,15 @@
 import { defineConfig } from 'sanity'
-import { deskTool} from 'sanity/desk'
+import { deskTool } from 'sanity/desk'
 import { visionTool } from '@sanity/vision'
 import { schemaTypes } from './schemas'
+import { vercelDeployTool } from 'sanity-plugin-vercel-deploy'
 
 export default defineConfig({
   name: 'bear-fired-fodder-backend',
   title: 'Bear Fired Fodder',
   projectId: '6f642s5x',
   dataset: 'production',
-  plugins: [deskTool(), visionTool()],
+  plugins: [deskTool(), visionTool(), vercelDeployTool()],
   schema: {
     types: schemaTypes,
   },
