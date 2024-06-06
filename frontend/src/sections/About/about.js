fb5866e7f14c161b271595f795e3908e2ce99d00
laura-richer <lauraricher@gmail.com>
2024-06-06 16:14:27 +0100

    chore: moving js files

--- frontend/src/sections/About/about.js
+++
@@ -1,5 +0,0 @@
-import showSection from '@/utils/show-section';
-
-export default () => {
-  showSection('.about__content');
-};
--- frontend/src/sections/Contact/contact.js
+++
@@ -1,5 +0,0 @@
-import showSection from '@/utils/show-section';
-
-export default () => {
-  showSection('.contact__container');
-};
