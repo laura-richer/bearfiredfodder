9929086fd662e4ca7a8abdeb1be9ada1e7fb1063
laura-richer <lauraricher@gmail.com>
2024-06-25 18:52:51 +0100

    chore: adding sitemap

--- frontend/astro.config.mjs
+++ frontend/astro.config.mjs
@@ -1,5 +1,8 @@
 import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
   compressHTML: true,
+  site: 'https:/www.bearfiredfodder.co.uk',
+  integrations: [sitemap()],
 });
